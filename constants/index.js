feb38ef57876f18f2628d27039194cd73fb4cfac
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-17 19:25:54 -0500

    Added social media accounts

---
+++ constants/index.js
@@ -0,0 +1,19 @@
+import { AiFillGithub, AiFillTwitterCircle, AiFillLinkedin } from 'react-icons/ai'
+
+export const socialMedia = [
+    {
+        name: 'GitHub',
+        icon: <AiFillGithub/>,
+        link: 'https://github.com/7thSamurai',
+    },
+    {
+        name: 'Twitter',
+        icon: <AiFillTwitterCircle/>,
+        link: 'https://twitter.com/zcollinsdev',
+    },
+    {
+        name: 'LinkedIn',
+        icon: <AiFillLinkedin/>,
+        link: 'https://linkedin.com/in/zcol',
+    },
+];

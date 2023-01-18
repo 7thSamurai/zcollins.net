0da4f703061426a8589d46e7c747c7ee52b2e3cb
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-18 10:55:35 -0500

    Made contact info fit better on small screens

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,13 +7,13 @@ function Contact() {
       <h2 className='text-3xl mb-6'>Contact Information</h2>
 
       <p className='text-xl text-center mb-6 max-w-lg mx-auto'>
-        If you have a project in mind, or simply have a few questions, please get in touch with me using one of the following:
+        If you have a project in mind, or simply have a few questions, please get in touch with me by using one of the following:
       </p>
 
       <div className='flex flex-row items-center'>
         <div>
           {contactInfo.map((contact) => (
-            <h3 className='flex flex-row text-2xl items-center'>
+            <h3 className='flex flex-row text-xl md:text-2xl items-center'>
               {contact.icon} &nbsp; {contact.name}:
             </h3>
           ))}
@@ -21,7 +21,7 @@ function Contact() {
 
         <div className='ml-4'>
           {contactInfo.map((contact) => (
-            <h3 className='text-2xl'><a className='text-teal-600' href={contact.info}>{contact.text}</a></h3>
+            <h3 className='text-xl md:text-2xl'><a className='text-teal-600' href={contact.info}>{contact.text}</a></h3>
           ))}
         </div>
       </div>

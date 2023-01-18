3c649d917b2d5fc24730259649d31f9a295a2937
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-17 19:26:51 -0500

    Added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { socialMedia } from '@/constants'
+
+const Footer = () => (
+  <section className='flex-col md:flex-row mb-8 w-full'>
+    <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-gray-300'>
+      <div>
+        <p className='text-center'>
+          Copyright &copy; 2023 Zachary Collins
+        </p>
+      </div>
+
+      <div className='flex flex-row mt-6 md:mt-0 text-gray-600'>
+        {socialMedia.map((social, index) => (
+          <a key={social.name} title={social.name} href={social.link} className={`text-[21px] object-contain ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`}>
+            {social.icon}
+          </a>
+        ))}
+      </div>
+    </div>
+  </section>
+)
+
+export default Footer

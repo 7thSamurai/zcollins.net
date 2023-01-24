18555ece40c2339763f3cbc8c7a24e97f3336eec
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-24 17:49:55 -0500

    Added title tag to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,13 @@
+import Head from "next/head"
 import styles from "../styles"
 
 export default function Custom404() {
   return (
     <div className={`${styles.paddingX}`}>
+      <Head>
+        <title>Zach Collins - 404</title>
+      </Head>
+
       <h1 className='font-bold text-4xl text-center py-20'>
         404 - Page Not Found
       </h1>

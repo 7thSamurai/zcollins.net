83978fcc4af8db90160180fe60fc7fa79285f0bf
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-24 17:34:26 -0500

    Added 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import styles from "../styles"
+
+export default function Custom404() {
+  return (
+    <div className={`${styles.paddingX}`}>
+      <h1 className='font-bold text-4xl text-center py-20'>
+        404 - Page Not Found
+      </h1>
+    </div>
+  )
+}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,12 +1,26 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Navbar, Footer } from '../components'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta name='description' content='Zachary Collins personal portfolio'/>
+        <meta name='viewport' content='width=device-width, initial-scale=1'/>
+        <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png'/>
+        <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png'/>
+        <link rel='icon' type='image/png' sizes='96x96' href='/favicon-96x96.png'/>
+      </Head>
+
       <body className='bg-ghost-white'>
-        <Main />
-        <NextScript />
+        <Navbar/>
+
+        <div className='flex flex-col justify-between min-h-screen'>
+          <Main/>
+          <Footer/>
+        </div>
+
+        <NextScript/>
       </body>
     </Html>
   )

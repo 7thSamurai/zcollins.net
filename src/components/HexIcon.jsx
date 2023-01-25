b893e00a1d680965967c9b0e43fe2089f9ad69af
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-01-24 19:02:49 -0500

    Redid hex icon

--- src/components/HexIcon.jsx
+++ src/components/HexIcon.jsx
@@ -3,14 +3,12 @@ import { BsHexagonFill } from 'react-icons/bs'
 
 /* Hexagon Icon */
 const HexIcon = (props) => (
-  <div className={`grid grid-cols-1 grid-rows-1 items-center ${props.styles}`}>
+  <div className={`relative ${props.styles}`}>
     {/* Hexagon background */}
-    <BsHexagonFill className='text-8xl row-start-1 col-start-1 text-keppel'/>
+    <BsHexagonFill className='text-8xl text-keppel block'/>
 
     {/* Actual icon */}
-    <div className='flex row-start-1 col-start-1 justify-center'>
-      <h2 className='text-6xl text-white'>{props.icon}</h2>
-    </div>
+    <h2 className='text-6xl text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>{props.icon}</h2>
   </div>
 )
 

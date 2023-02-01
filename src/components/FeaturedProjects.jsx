49ed45d6af505d007cfc881b6c1c4a73a852ab92
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-02-01 16:58:45 -0500

    Added description to article pages

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 import { ArticlePreview, Heading } from '.'
 import { projects_description } from '../constants'
 
 const FeaturedProjects = (props) => (
   <section className='bg-ghost-white'>
-    <Heading.H2 className='text-center pt-10'>Featured Projects</Heading.H2>
+    <Heading.H2 className='text-center pt-10'>
+      <Link href='/projects' title='View all Projects'>Featured Projects</Link>
+    </Heading.H2>
 
     <p className='text-center text-xl my-4 max-w-2xl mx-auto'>
       {projects_description}
--- src/components/LatestBlogPosts.jsx
+++ src/components/LatestBlogPosts.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 import { ArticlePreview, Heading } from '.'
 import { blog_description } from '../constants'
 
 const LatestBlogPosts = (props) => (
   <section className='bg-ghost-white'>
-    <Heading.H2 className='text-center pt-10'>Latest Blog Posts</Heading.H2>
+    <Heading.H2 className='text-center pt-10'>
+      <Link href='/blog' title='View all Blog Posts'>Latest Blog Posts</Link>
+    </Heading.H2>
 
     <p className='text-center text-xl my-4 max-w-2xl mx-auto'>
       {blog_description}

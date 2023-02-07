2aaaf06da1d9240c7046bdf80321520ae8a99c19
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-02-07 16:35:14 -0500

    Added view all link to blog and projects

--- src/components/FeaturedProjects.jsx
+++ src/components/FeaturedProjects.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import { ArticlePreview, Heading } from '.'
 import { projects_description } from '../constants'
+import { BsArrowRight } from 'react-icons/bs'
 
 const FeaturedProjects = (props) => (
-  <section className='bg-ghost-white'>
+  <section className='bg-ghost-white flex flex-col'>
     <Heading.H2 className='text-center'>
       <Link href='/projects' title='View all Projects'>Featured Projects</Link>
     </Heading.H2>
@@ -14,6 +15,11 @@ const FeaturedProjects = (props) => (
     </p>
 
     <ArticlePreview path='/projects' {...props}/>
+
+    <Link href='/projects' className='justify-center inline-flex items-center text-xl text-keppel hover:underline hover:text-teal-600'>
+      View all Featured Projects
+      <BsArrowRight className='ml-2'/>
+    </Link>
   </section>
 )
 
--- src/components/LatestBlogPosts.jsx
+++ src/components/LatestBlogPosts.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 import Link from 'next/link'
 import { ArticlePreview, Heading } from '.'
 import { blog_description } from '../constants'
+import { BsArrowRight } from 'react-icons/bs'
 
 const LatestBlogPosts = (props) => (
-  <section className='bg-ghost-white'>
+  <section className='bg-ghost-white flex flex-col'>
     <Heading.H2 className='text-center pt-10'>
       <Link href='/blog' title='View all Blog Posts'>Latest Blog Posts</Link>
     </Heading.H2>
@@ -14,6 +15,11 @@ const LatestBlogPosts = (props) => (
     </p>
 
     <ArticlePreview path='/blog' {...props}/>
+
+    <Link href='/projects' className='justify-center inline-flex items-center text-xl text-keppel hover:underline hover:text-teal-600'>
+      View all Blog Projects
+      <BsArrowRight className='ml-2'/>
+    </Link>
   </section>
 )
 

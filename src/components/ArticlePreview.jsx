4892f611f80e9d03bf4efc61bbecc237a47441cf
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-02-07 21:44:08 -0500

    Redid article preview card

--- src/components/ArticlePreview.jsx
+++ src/components/ArticlePreview.jsx
@@ -6,29 +6,31 @@ function ArticlePreview(props) {
   return (
     <div className='grid grid-cols-1 lg:grid-cols-3 py-10 gap-8'>
       {props.articles.map((article) => (
-        <Link key={article.meta.title} href={`${props.path}/${article.meta.slug}`} className='flex flex-col justify-between border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 cursor-pointer p-4'>
-          <div>
-            {/* Article thumbnail image */}
-            <img src={`/images/${article.meta.image}`} className='w-full rounded-lg' alt=''/>
+        <Link key={article.meta.title} href={`${props.path}/${article.meta.slug}`} className='flex flex-col border border-gray-200 rounded-lg shadow-lg hover:bg-gray-100 cursor-pointer'>
+          {/* Article thumbnail image */}
+          <img src={`/images/${article.meta.image}`} className='w-full rounded-t-lg' alt=''/>
 
-            {/* Article type */}
-            <p className='text-keppel text-lg my-4'>
-              {article.meta.type}
-            </p>
+          <div className='p-4 flex flex-col justify-between h-full'>
+            <div className='justify-start'>
+              {/* Article type */}
+              <p className='text-keppel text-lg mb-2 font-medium'>
+                {article.meta.type}
+              </p>
 
-            <Heading.H3>{article.meta.title}</Heading.H3 >
-          </div>
+              <Heading.H3>{article.meta.title}</Heading.H3>
+            </div>
+            
+            <div className='justify-end'>
+              {/* Brief Article description */}
+              <p className='text-xl mt-4 text-eerie-black'>
+                {article.meta.about}
+              </p>
 
-          {/* Brief Article description */}
-          <p className='text-xl mt-4 text-eerie-black'>
-            {article.meta.about}
-          </p>
-
-          <div className='flex flex-wrap justify-between mt-8'>
-            {/* Reading time */}
-            <p className='text-lg text-roman-silver mr-8 flex-shrink-0'>
-              {article.meta.readingTime} &middot; {new Date(article.meta.publishedAt).toLocaleDateString('en-us', { year:"numeric", month:"short", day: "2-digit"})}
-            </p>
+              {/* Reading time */}
+              <p className='text-lg text-roman-silver mt-8 flex-shrink-0'>
+                {article.meta.readingTime} &middot; {new Date(article.meta.publishedAt).toLocaleDateString('en-us', { year:"numeric", month:"short", day: "numeric"})}
+              </p> 
+            </div>
           </div>
         </Link>
       ))}

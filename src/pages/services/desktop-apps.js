ad3ad91bdcd0f5c6e316f0f830fef22697b128c4
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-02-14 18:06:42 -0500

    Added desktop apps service content

--- src/pages/services/desktop-apps.js
+++ src/pages/services/desktop-apps.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import { Service, Heading } from '@/src/components';
+import Head from 'next/head';
 
 function DesktopApps() {
   return (
-    <div>DesktopApps</div>
+    <Service title='Custom Desktop Application Development'>
+      <p className='py-2'>
+        In today's modern and rapidly changing business world, companies often employ custom software solutions to help automate processes, boost efficiency, and streamline their internal operations. 
+        In many cases, this can be achieved by using custom-built desktop applications. 
+        A desktop application is a type of specialized  computer software that runs on PCs and laptops. 
+        These applications can be specifically designed and developed for a particular client per their requirements to meet their unique needs.
+      </p>
+      <p className='py-2'>
+        As a skilled software developer, I offer services to design and implement high-quality software solutions that help businesses increase productivity and achieve their objectives. 
+        Whether you need a completely new application built from the ground up, 
+        or you have an existing piece of software that needs additional functionality added, I have the knowledge and expertise to provide the optimal solution tailored to your specific business needs.
+      </p>
+
+      <Heading.H2 className='my-4'>Automation</Heading.H2>
+      <p className='py-2'>
+        Custom desktop applications can be used to automate many different types of repetitive computer tasks, freeing up your valuable time to focus on more critical and essential duties. 
+        For example, a custom desktop application could be used for automatic data entry, report generation, or inventory management. 
+        In your business alone, it's highly likely that there are numerous everyday tasks that could be automated away, thus allowing your employees to work more efficiently and productively.
+      </p>
+
+      <Heading.H2 className='my-4'>Scalability</Heading.H2>
+      <p className='py-2'>
+        Custom desktop applications can be designed to grow along with your business. 
+        As your company expands and evolves, your custom software solutions can easily be updated and tailored to meet your ever-changing needs and requirements. 
+        This scalability helps to ensure that your business will always have the ideal tools it needs to succeed, and not be held back by the limitations that go along with prebuilt software.
+      </p>
+
+      <Heading.H2 className='my-4'>Multi-Platform Development</Heading.H2>
+      <p className='py-2'>
+        One useful benefit of custom-built desktop applications is that if necessary, they can be designed to work on a variety of different platforms and environments, including <i>Windows</i>, <i>Mac</i>, and <i>Linux-based</i> systems.
+        This proves to be extremely helpful if your business utilizes multiple system types, so you may smoothly deploy and operate your software application across all your computers!
+      </p>
+
+      <Heading.H2 className='my-4'>Improved User Experience</Heading.H2>
+      <p className='py-2'>
+        Custom desktop applications can be designed with your employees in mind, ensuring a user-friendly interface, an intuitive layout, and straightforward navigation, completely customized to your business's needs and usage.
+        This helps to ensure an enjoyable user experience, which results in increased productivity and satisfaction among your employees.
+      </p>
+
+      <Heading.H2 className='my-4'>Conclusion</Heading.H2>
+      <p className='py-2'>
+        Custom desktop applications offer businesses numerous benefits and improvements, including but not limited to task automation, multi-platform applications, software scalability, and improved user experience. 
+        As a experienced software developer, I offer effective and robust software solutions that are tailored to your business's specific needs and use cases, helping you achieve your goals and stay one step ahead of the competition.
+      </p>
+
+      <p className='py-2'>
+        If you're looking to streamline your operations and increase efficiency, please do not hesitate to contact me to discuss and learn more about how I may assist your business by providing specialized software solutions via custom desktop applications!
+      </p>
+    </Service>
   );
 }
 

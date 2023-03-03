cbd0efd703dc9825959d2eac61f2eb5f00f7905c
Zach Collins <76573299+7thSamurai@users.noreply.github.com>
2023-03-03 16:34:33 -0500

    Rewrote the database design page

--- src/pages/services/database-design.js
+++ src/pages/services/database-design.js
@@ -5,25 +5,21 @@ function DatabaseDesign() {
   return (
     <Service title='Database Design' meta='Freelance Software Developer offering custom database design and implementation services for business applications.'>
       <p className='py-2'>
-        In this modern high-tech age, business heavily rely on using computers for storing, organizing, and processing vast amounts of data to keep their operations running.
-        Computers achieve this great technological feat by employing extremely complex and advance software systems known as databases.
-        There are many such database systems available, such as <b>Oracle&apos;s MySQL</b> or <b>Microsoft&apos;s SQL Server</b>, but the exact system your business requires will depend on your unique requirements and use cases.
+        In this modern high-tech age, business heavily rely on using computers running database software for storing, organizing, and processing vast amounts of data to keep their operations running.
       </p>
       <p className='py-2'>
-        If you are unsure what type of database system you need, or if you don&apos;t even know if it&apos;s possible accomplish your requirements, feel free to contact me so that we may discuss your situation and create a game plan that works with you!
+        My services include <i>designing</i>, <i>implementing</i>, <i>maintaining</i>, and <i>analyzing</i> databases, whether starting from scratch, or modifying an existing database, I have the skills and knowledge to get the job done!
       </p>
 
-      <Heading.H2 className='my-4'>Relational Databases</Heading.H2>
+      <Heading.H2 className='my-8'>Service Breakdown</Heading.H2>
+
+      <Heading.H3 className='my-2'>Software Selection</Heading.H3>
       <p className='py-2'>
-        One of the most widely used varieties of databases is known as a <b>Relational Database Management System</b> (<b>RDBMS</b>).
-        These databases are designed to efficiently store data that is interconnected together in a relational manner.
-        They also usually includes the ability to manipulate and access it&apos;s data via a special type of programming language known as <b>Structured Query Language</b> (<b>SQL</b>), hence the reason why many such systems have &quot;SQL&quot; in their name.
-      </p>
-      <p className='py-2'>
-        Due to their highly robust and versatile nature, a SQL database will likely be the ideal choice for your situation and needs.
+        There are many database systems available, such as <a className='hover:text-black hover:underline font-bold' href='https://www.mysql.com/'>Oracle&apos;s MySQL</a> or <a className='hover:text-black hover:underline font-bold' href='https://www.microsoft.com/en-us/sql-server'>Microsoft&apos;s SQL Server</a>, but the exact system your business requires will depend on your unique requirements and use cases.
+        As part of my services I will discuss with you your requirements and assisting you with determining the type and the exact variety of database which is best suited for your specific needs, whether it&apos;s a traditional <b>Relational Database Management System</b> (<b>RDBMS</b>), or a more recent <b>Non-Relational</b> (<b>NoSQL</b>) database.
       </p>
 
-      <p>A few of the most well known examples of relational databases are:</p>
+      <p>Some examples of relational databases that I can help you with are:</p>
       <ul className='list-disc ml-8 pb-2'>
         <li><a className='hover:text-black hover:underline' href='https://www.mysql.com/'>MySQL</a></li>
         <li><a className='hover:text-black hover:underline' href='https://www.microsoft.com/en-us/sql-server'>Microsoft SQL Server</a></li>
@@ -31,16 +27,7 @@ function DatabaseDesign() {
         <li><a className='hover:text-black hover:underline' href='https://mariadb.com/'>MariaDB</a></li>
       </ul>
 
-      <Heading.H2 className='my-4'>NoSQL Databases</Heading.H2>
-      <p className='py-2'>
-        <b>NoSQL</b> databases are a relatively recent creation that attempt to overcome some of the limitations and difficulties that come with traditional relational databases in certain situations.
-        These non-relational databases are often referred to as &quot;NoSQL databases&quot; due to their absence of the SQL programming language previously mentioned.
-      </p>
-      <p className='py-2'>
-        While not the correct substitute for all cases, NoSQL databases do pose a significant advantage over normal SQL databases in certain scenarios where there is a constant high-volume flow of non-interconnected data.
-      </p>
-      
-      <p>Some of the most well known examples of non-relational databases are:</p>
+      <p>And some examples of non-relational databases are:</p>
       <ul className='list-disc ml-8 pb-2'>
         <li><a className='hover:text-black hover:underline' href='https://www.mongodb.com/'>MonogoDB</a></li>
         <li><a className='hover:text-black hover:underline' href='https://cassandra.apache.org/'>Cassandra</a></li>
@@ -48,15 +35,35 @@ function DatabaseDesign() {
         <li><a className='hover:text-black hover:underline' href='https://neo4j.com/'>Neo4j</a></li>
       </ul>
 
-      <Heading.H2 className='my-4'>Spreadsheet Migration</Heading.H2>
+      <Heading.H3 className='my-2'>Design</Heading.H3>
       <p className='py-2'>
-        Many business rely on using spreadsheets for organizing and processing their data. 
-        While this may work just fine at a small scale, when your operation grows and expands, the limitations and complications that come with this solution will quickly became apparent.
+        One we have selected a database, we may begin the design process. 
+        Here I will work with to determine all the data that needs to be stored, and all the necessary relations that need to be created.
+        With this knowledge I will then start working on designing the layout of how the information will actually be organized in the database.
       </p>
       <p className='py-2'>
-        To overcome these dilemmas, a database system can be employed to house your data instead.
-        How you access this data is totally up to you, whether you want a custom desktop application, a web interface, or something else entirely, it&apos;s at your discretion.
-        Your existing spreadsheet may also be easily imported and transferred to the new database without any loss of your current data and records.
+        Additionally, I will discuss with you the various hosting options available, such as <a className='hover:text-black hover:underline font-bold' href='https://aws.amazon.com/'>Amazon's AWS</a> or <a className='hover:text-black hover:underline font-bold' href='https://azure.microsoft.com/'>Microsoft's Azure</a>, and which one best suites your needs and your budget.
+      </p>
+
+      <Heading.H3 className='my-2'>Implementation</Heading.H3>
+      <p className='py-2'>
+        After I've designed the database, I will help you setup any necessary hosting accounts, and get a database instance running.
+        Then I will handle running all the necessary commands to start creating and populating the database tables.
+      </p>
+      <p className='py-2'>
+        Additionally, if you already have some existing data, I will also clean, organize, and import it into the new system for you!
+      </p>
+
+      <Heading.H3 className='my-2'>Maintenance</Heading.H3>
+      <p className='py-2'>
+        My services also include modifying, debugging, and upgrading your existing database systems.
+        If you have additional features that need added, bugs that need fixed, or if you even need to migrate your database to another hosting provider, I got you covered.
+      </p>
+
+      <Heading.H3 className='my-2'>Data Analyzation</Heading.H3>
+      <p className='py-2'>
+        As part of my services I will also assist you with data analysis.
+        If you need to extract and analyze portions of your data, I can assist you by writing the necessary SQL query statements to select the data, and then further analyze and manipulate it by using technologies such as <b>Python</b> and <b>Pandas</b>.
       </p>
     </Service>
   );
